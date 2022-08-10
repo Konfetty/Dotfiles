036f5665af38947e0983d485fd58db050a069d48
Aljoscha Eidloth <aljoscha.eidloth@googlemail.com>
2022-08-10 11:30:33 +0200

    add shortcut and script to open spotify and monitoring tools for work on workspace 1

---
+++ dot_config/sway/scripts/monitoringWithSpotifyOn1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+swaymsg workspace 1
+swaymsg splith
+sleep 0.2
+spotify &
+sleep 0.2
+alacritty -e btop &
+sleep 0.2
+swaymsg splitv
+alacritty --working-directory ~/work/backend &
+sleep 0.2
+swaymsg splith
+alacritty --working-directory ~/work/backend &
+sleep 0.2
+swaymsg layout tabbed
